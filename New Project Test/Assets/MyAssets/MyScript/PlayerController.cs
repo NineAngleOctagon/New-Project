6a34b9f4e80e4aa75cb08c79547835cbb3bb109d
lenchanteur42 <32389013+lenchanteur42@users.noreply.github.com>
2018-01-27 11:48:25 +0100

    New Project First
    Ground : Square
    Player : Sphere
    Movement : Forward only (Forces)
    Camera : Following Player
    No Network

---
+++ New Project Test/Assets/MyAssets/MyScript/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce;
+
+    void Start()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+    }
+
+    // We marked this as "FixedPoint" before we are using it to mess with physics
+    void FixedUpdate ()
+    {
+        
+  	}
+}
