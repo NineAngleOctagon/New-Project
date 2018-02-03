46e9788004a4cd1a51ee1c3f2c72105344d31f9b
Steakkk <32322396+Steakkk@users.noreply.github.com>
2018-02-03 22:13:45 +0100

    mouvement diagonale
    mouvement en diagonales (a & e) codés

--- New Project mouv dg/New Project Bidouillage Mouv'/Assets/MyAssets/MyScript/PlayerController.cs
+++ New Project mouv dg/New Project Bidouillage Mouv'/Assets/MyAssets/MyScript/PlayerController.cs
@@ -21,55 +21,155 @@ public class PlayerController : NetworkBehaviour {
 		if (!isLocalPlayer)
 			return;
 
-		if (Input.GetKey("d"))
+        if (Input.GetKey("d") && rb.position.y == 0.5 && Time.time - tps >= gap)
 		{
-			if (rb.velocity == new Vector3(0, rb.velocity.y, moveSpeed) && Time.time - tps >= gap)
-			{
-				rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
-				tps = Time.time;
-			}
-			else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, 0) && Time.time - tps >= gap)
-			{
-				rb.velocity = new Vector3(0, rb.velocity.y, -moveSpeed);
-				tps = Time.time;
-			}
-			else if (rb.velocity == new Vector3(0, rb.velocity.y, -moveSpeed) && Time.time - tps >= gap)
+            if (rb.velocity == new Vector3(0, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);                
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, 0))
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(0, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, 0))
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            tps = Time.time;
+        }
+
+		if (Input.GetKey("q") && rb.position.y == 0.5 && Time.time - tps >= gap)
+		{
+			if (rb.velocity == new Vector3(0, rb.velocity.y, moveSpeed))
 			{
 				rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
-				tps = Time.time;
 			}
-			else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, 0) && Time.time - tps >= gap)
-			{
-				rb.velocity = new Vector3(0, rb.velocity.y, moveSpeed);
-				tps = Time.time;
-			}
-		}
-
-		if (Input.GetKey("q"))
-		{
-			if (rb.velocity == new Vector3(0, rb.velocity.y, moveSpeed) && Time.time - tps >= gap)
-			{
-				rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
-				tps = Time.time;
-			}
-			else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, 0) && Time.time - tps >= gap)
+			else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, 0))
 			{
 				rb.velocity = new Vector3(0, rb.velocity.y, -moveSpeed);
-				tps = Time.time;
 			}
-			else if (rb.velocity == new Vector3(0, rb.velocity.y, -moveSpeed) && Time.time - tps >= gap)
+			else if (rb.velocity == new Vector3(0, rb.velocity.y, -moveSpeed))
 			{
 				rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
-				tps = Time.time;
 			}
-			else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, 0) && Time.time - tps >= gap)
+			else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, 0))
 			{
 				rb.velocity = new Vector3(0, rb.velocity.y, moveSpeed);
-				tps = Time.time;
 			}
-		}
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            tps = Time.time;
+        }
 
-		if (Input.GetKey(KeyCode.Space) && rb.position.y == 0.5)
+        if (Input.GetKey("e") && rb.position.y == 0.5 && Time.time - tps >= gap)
+        {
+            if (rb.velocity == new Vector3(0, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, 0))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(0, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, 0))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, moveSpeed);
+            }
+            tps = Time.time;
+        }
+
+        if (Input.GetKey("a") && rb.position.y == 0.5 && Time.time - tps >= gap)
+        {
+            if (rb.velocity == new Vector3(0, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, 0))
+            {
+                rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(-moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(0, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, -moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, -moveSpeed))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, 0))
+            {
+                rb.velocity = new Vector3(moveSpeed, rb.velocity.y, moveSpeed);
+            }
+            else if (rb.velocity == new Vector3(moveSpeed, rb.velocity.y, moveSpeed))
+            {
+                rb.velocity = new Vector3(0, rb.velocity.y, moveSpeed);
+            }
+            tps = Time.time;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && rb.position.y == 0.5)
 		{
 			rb.AddForce(0, jumpForce, 0);
 		}
