b283bfcb6678e5489459dd704d22e548479ee21c
lenchanteur42 <32389013+lenchanteur42@users.noreply.github.com>
2018-03-25 16:57:25 +0200

    New Project Matrix
    J'ai échangé ls cubes pour une matrice qui stocke les positions occupées par le player. Selon mes tests, ça fonctionne à 100% en solo, mais ça a pas l'air de marcher en multi, donc à vérifier.

--- New Project/Assets/MyAssets/MyScript/GameOver.cs
+++ New Project/Assets/MyAssets/MyScript/GameOver.cs
@@ -8,14 +8,32 @@ public class GameOver : NetworkBehaviour {
     public Camera EndCam;
     public bool isOver;
 
+    private int[,] board;
+    private TrailRenderer trail;
+    public float gapTrail;
+    private float tpsTrail;
+
+    private void Start()
+    {
+        tpsTrail = Time.time + 0.1f;
+        board = new int[300, 300];
+        for (int i = 0; i < 300; i++)
+        {
+            for (int j = 0; j < 300; j++)
+            {
+                board[i, j] = -1;
+            }
+        }
+    }
+
     private void Update()
     {
-        if (rb.position.y <= -15)
+        if (board[(int) rb.transform.position.x + 150, (int) rb.transform.position.z + 150] == (int) rb.transform.position.y && !isOver)
         {
-            isOver = false;
+            isOver = true;
 
             rb.GetComponent<PlayerController>().moveSpeed = 0;
-            rb.GetComponent<WallCreater>().gapTrail = float.MaxValue;
+            gapTrail = float.MaxValue;
             rb.isKinematic = true;
 
             PlayerCam.enabled = false;
@@ -24,16 +42,22 @@ public class GameOver : NetworkBehaviour {
             EndCam.transform.rotation = new Quaternion(0f, -0.7071f, 0.7071f, 0f);
             EndCam.enabled = true;
         }
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Cube")
+        if (Time.time - tpsTrail >= 0)
+        {
+            trail = rb.GetComponent<TrailRenderer>();
+
+            board[(int) trail.GetPosition(trail.positionCount - 2).x + 150, (int) trail.GetPosition(trail.positionCount - 2).z + 150] = (int) trail.GetPosition(trail.positionCount - 2).y;
+
+            tpsTrail = Time.time;
+        }
+
+        if (rb.position.y <= -15 && !isOver)
         {
             isOver = true;
 
             rb.GetComponent<PlayerController>().moveSpeed = 0;
-            rb.GetComponent<WallCreater>().gapTrail = float.MaxValue;
+            gapTrail = float.MaxValue;
             rb.isKinematic = true;
 
             PlayerCam.enabled = false;
