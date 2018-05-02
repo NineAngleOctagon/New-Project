1cc147daf97bfd64933559574d245128dbabd1ec
Steakkk <32322396+Steakkk@users.noreply.github.com>
2018-05-02 12:39:32 +0200

    Caméra de mort
    pavé numérique : 1, 2, 3 pour changer de map avec la caméra de mort

---
+++ New Project/Assets/MyAssets/MyScript/GameOverController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameOverController : NetworkBehaviour {
+
+    private bool isOver;
+    private Camera PlayerCam;
+
+    private void Start()
+    {
+        PlayerCam = GetComponent<GameOver>().PlayerCam;
+    }
+
+    void Update () {
+        isOver = GetComponent<GameOver>().isOver;
+        if (isOver)
+        {
+            if (Input.GetKey("[1]"))
+                PlayerCam.transform.position = new Vector3(0, 150, 0);
+            if (Input.GetKey("[2]"))
+                PlayerCam.transform.position = new Vector3(-300, 150, -200);
+            if (Input.GetKey("[3]"))
+                PlayerCam.transform.position = new Vector3(200, 150, -300);
+        }
+	}
+}
