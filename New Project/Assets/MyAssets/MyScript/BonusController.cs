eb80d4a0977661d8f549daca48ac157d59c103e7
Steakkk <32322396+Steakkk@users.noreply.github.com>
2018-06-04 12:33:19 +0200

    bonus multi
    bonus même position multijoueur

--- New Project/Assets/MyAssets/MyScript/BonusController.cs
+++ New Project/Assets/MyAssets/MyScript/BonusController.cs
@@ -25,53 +25,62 @@ public class BonusController : NetworkBehaviour
 
         if (Time.time - tpsBonus >= gapBonus && !rb.GetComponent<GameOver>().isOver)
         {
-            float platform = Random.Range(0.0f, 3.0f);
-
-            float posx;
-            float posz;
-
-            switch ((int)platform)
-            {
-                case 0:
-                    posx = Random.Range(-75.0f, 75.0f);
-                    posz = Random.Range(-75.0f, 75.0f);
-                    break;
-                case 1:
-                    posx = Random.Range(-375.0f, -225.0f);
-                    posz = Random.Range(-275.0f, -125.0f);
-                    break;
-                case 2:
-                    posx = Random.Range(125.0f, 275.0f);
-                    posz = Random.Range(-375.0f, -225.0f);
-                    break;
-                default:
-                    posx = Random.Range(-70.0f, 70.0f);
-                    posz = Random.Range(-70.0f, 70.0f);
-                    break;
-            }
-
-            Vector3 position = new Vector3(posx, 1.0f, posz);
-
-            int bonusType = (int)Random.Range(0f, 4.0f);
-            switch (bonusType)
-            {
-                case 0:
-                    Instantiate(bonus1, position, Quaternion.identity);
-                    break;
-                case 1:
-                    Instantiate(bonus2, position, Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(bonus3, position, Quaternion.identity);
-                    break;
-                case 3:
-                    Instantiate(bonus4, position, Quaternion.identity);
-                    break;
-                default:
-                    break;
-            }
+            CmdSpawn();
 
             tpsBonus = Time.time;
+        }
+    }
+
+    [Command]
+    void CmdSpawn()
+    {
+        float platform = Random.Range(0.0f, 3.0f);
+
+        float posx;
+        float posz;
+
+        switch ((int)platform)
+        {
+            case 0:
+                posx = Random.Range(-75.0f, 75.0f);
+                posz = Random.Range(-75.0f, 75.0f);
+                break;
+            case 1:
+                posx = Random.Range(-375.0f, -225.0f);
+                posz = Random.Range(-275.0f, -125.0f);
+                break;
+            case 2:
+                posx = Random.Range(125.0f, 275.0f);
+                posz = Random.Range(-375.0f, -225.0f);
+                break;
+            default:
+                posx = Random.Range(-70.0f, 70.0f);
+                posz = Random.Range(-70.0f, 70.0f);
+                break;
+        }
+
+        Vector3 position = new Vector3(posx, 1.0f, posz);
+        GameObject bonus;
+
+        int bonusType = (int)Random.Range(0f, 4.0f);
+        switch (bonusType)
+        {
+            case 0:
+                bonus = (GameObject) Instantiate(bonus1, position, Quaternion.identity);
+                NetworkServer.Spawn(bonus);
+                break;
+            case 1:
+                bonus = (GameObject) Instantiate(bonus2, position, Quaternion.identity);
+                NetworkServer.Spawn(bonus);
+                break;
+            case 2:
+                bonus = (GameObject) Instantiate(bonus3, position, Quaternion.identity);
+                NetworkServer.Spawn(bonus);
+                break;
+            default:
+                bonus = (GameObject) Instantiate(bonus4, position, Quaternion.identity);
+                NetworkServer.Spawn(bonus);
+                break;
         }
     }
 }
