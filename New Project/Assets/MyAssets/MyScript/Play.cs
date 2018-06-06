c9e6d9b48d269d9993584ceebaaa8333e4b0255b
lenchanteur42 <32389013+lenchanteur42@users.noreply.github.com>
2018-06-06 18:17:45 +0200

    New Project Menu
    New Project avec le menu moche et les scènes solo pas complète

---
+++ New Project/Assets/MyAssets/MyScript/Play.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+    public GameObject OptionMenu;
+
+    public Dropdown numAI;
+    public Dropdown diffAI;
+
+    public void PlaySolo()
+    {
+        switch (numAI.value)
+        {
+            case 0:
+                if (diffAI.value == 0)
+                    SceneManager.LoadScene("Solo1AI1");
+                else
+                    SceneManager.LoadScene("Solo1AI2");
+                break;
+            case 1:
+                if (diffAI.value == 0)
+                    SceneManager.LoadScene("Solo2AI1");
+                else
+                    SceneManager.LoadScene("Solo2AI2");
+                break;
+            default:
+                if (diffAI.value == 0)
+                    SceneManager.LoadScene("Solo3AI1");
+                else
+                    SceneManager.LoadScene("Solo3AI2");
+                break;
+        }
+    }
+
+    public void PlayMulti()
+    {
+        SceneManager.LoadScene("lobbyScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
