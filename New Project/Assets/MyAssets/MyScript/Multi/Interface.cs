0be12d68b5f0de1890e538e28f3fa39b3b665488
TheUnknownWhy <pierre.houy@epita.fr>
2018-06-07 15:06:34 +0200

    interface menu

--- New Project/Assets/MyAssets/MyScript/Multi/Interface.cs
+++ New Project/Assets/MyAssets/MyScript/Multi/Interface.cs
@@ -28,11 +28,11 @@ public class Interface : NetworkBehaviour
         timerghost = rb.GetComponent<PlayerController>().tpsBonus3 - Time.time + 7;
         timerwalls = rb.GetComponent<PlayerController>().tpsBonus4 - Time.time + 7;
 
-        timersafe = rb.GetComponent<WallCreater>().tpsSafe - Time.time + 5;    
+        timersafe = rb.GetComponent<WallCreater>().tpsSafe - Time.time + 5;
     }
 
     public Texture robot;
-    private string robottalk = "";
+
 
     public GUISkin SafeZoneskin;
     public GUISkin Normalskin;
@@ -40,6 +40,13 @@ public class Interface : NetworkBehaviour
     public GUISkin Slowskin;
     public GUISkin Gohstskin;
     public GUISkin BigWallskin;
+
+    private bool Istalkingspeed = false;
+    private bool Istalkingslow = false;
+    private bool Istalkingghost = false;
+    private bool Istalkingbigwalls = false;
+
+
 
     private void OnGUI()
     {
@@ -51,51 +58,94 @@ public class Interface : NetworkBehaviour
             if (timerfast >= 0.0f)
             {
                 GUI.skin.box = Speedskin.box;
-                            
-                robottalk = "You will never be as fast as I am ! \n \n  Because I, and only I am the best !";
-                
-                GUI.Box(new Rect(Screen.width - 100, (Screen.height/2) + 10, 75,75), timerfast.ToString("0"));
-                GUI.skin.box = Normalskin.box;
-                GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), robottalk);
+                GUI.Box(new Rect(Screen.width - 100, (Screen.height / 2) + 10, 75, 75), timerfast.ToString("0"));
+
                 GUI.Box(new Rect((Screen.width - (Screen.height / 4) - 10), 0, Screen.height / 4, Screen.height / 4), robot);
+                if (!Istalkingbigwalls && !Istalkingghost && !Istalkingslow)
+                {
+                    Istalkingspeed = true;
+                    GUI.skin.box = Normalskin.box;
+                    GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), "You will never be as fast as I am ! \n \n  Because I, and only I am the best !");
+                }
+            }
+            else
+            {
+                Istalkingspeed = false;
             }
 
             if (timerslow >= 0.0f)
             {
                 GUI.skin.box = Slowskin.box;
-                robottalk = "Super secret Snail Jutstu !";
+
                 GUI.Box(new Rect(Screen.width - 100, (Screen.height / 2) - 75, 75, 75), timerslow.ToString("0"));
-                GUI.skin.box = Normalskin.box;
-                GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), robottalk);
+
+
                 GUI.Box(new Rect((Screen.width - (Screen.height / 4) - 10), 0, Screen.height / 4, Screen.height / 4), robot);
+                if (!Istalkingbigwalls && !Istalkingghost && !Istalkingspeed)
+                {
+                    Istalkingslow = true;
+                    GUI.skin.box = Normalskin.box;
+                    GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), "Super secret Snail Jutsu !");
+                }
+            }
+            else
+            {
+                Istalkingslow = false;
             }
 
             if (timerghost >= 0.0f)
             {
+
                 GUI.skin.box = Gohstskin.box;
-                robottalk = "YOU SHALL NOT ... \n \n  Oh well okay you got me...";
                 GUI.Box(new Rect(Screen.width - 100, (Screen.height / 2) + 95, 75, 75), timerghost.ToString("0"));
-                GUI.skin.box = Normalskin.box;
-                GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), robottalk);
+
                 GUI.Box(new Rect((Screen.width - (Screen.height / 4) - 10), 0, Screen.height / 4, Screen.height / 4), robot);
+
+                if (!Istalkingbigwalls && !Istalkingslow && !Istalkingspeed)
+                {
+                    Istalkingghost = true;
+                    GUI.skin.box = Normalskin.box;
+                    GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), "YOU SHALL NOT ... \n \n  Oh well okay you got me...");
+                }
+            }
+            else
+            {
+                Istalkingghost = false;
             }
 
             if (timerwalls >= 0.0f)
             {
                 GUI.skin.box = BigWallskin.box;
-                robottalk = "THEY SHALL NOT PASS !!";
+
                 GUI.Box(new Rect(Screen.width - 100, (Screen.height / 2) - 160, 75, 75), timerwalls.ToString("0"));
-                GUI.skin.box = Normalskin.box;
-                GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), robottalk);
+
+
                 GUI.Box(new Rect((Screen.width - (Screen.height / 4) - 10), 0, Screen.height / 4, Screen.height / 4), robot);
-            }      
+
+                if (!Istalkingghost && !Istalkingslow && !Istalkingspeed)
+                {
+                    Istalkingbigwalls = true;
+                    GUI.skin.box = Normalskin.box;
+                    GUI.Box(new Rect(Screen.width / 6, 0, Screen.width - (Screen.height / 4) - (Screen.width / 6) - 50, Screen.height / 8), "THEY SHALL NOT PASS !!");
+                }
+            }
+            else
+            {
+                Istalkingbigwalls = false;
+            }
+
+
+
+
         }
-        
         if (timersafe >= 0.0f && rb.GetComponent<WallCreater>().isSafe)
         {
             GUI.skin.box = SafeZoneskin.box;
-            
-            GUI.Box(new Rect(Screen.width - 100, (Screen.height / 2) -245 , 75, 75), timersafe.ToString("0"));
-        } 
-    }  
+
+            GUI.Box(new Rect(Screen.width - 100, (Screen.height / 2) - 245, 75, 75), timersafe.ToString("0"));
+        }
+
+    }
+
+
 }
