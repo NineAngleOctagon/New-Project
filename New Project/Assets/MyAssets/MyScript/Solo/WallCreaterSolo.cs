8bd79e4aa3f9ea291083750bb949805afea8db8b
Steakkk <32322396+Steakkk@users.noreply.github.com>
2018-06-07 14:27:47 +0200

    solo

---
+++ New Project/Assets/MyAssets/MyScript/Solo/WallCreaterSolo.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class WallCreaterSolo : MonoBehaviour
+{
+    public Rigidbody rb;
+    private TrailRenderer trail;
+    private int gapTrail;
+    private int frequency;
+    private int distance;
+    public GameObject originalCube;
+    public GameObject myBigCube;
+    private bool bigWall;
+
+    public bool isSafe = false;
+    public float tpsSafe;
+
+    private void Start()
+    {
+        gapTrail = 6;
+        frequency = 1;
+        distance = 6;
+    }
+
+    void Update()
+    {
+        bigWall = rb.GetComponent<PlayerControllerSolo>().bigWall;
+        trail = rb.GetComponent<TrailRenderer>();
+
+        if (trail.positionCount > gapTrail)
+        {
+            GameObject cube = originalCube;
+
+            if (bigWall)
+            {
+                cube = myBigCube;
+            }
+
+            Vector3 pos = trail.GetPosition(trail.positionCount - distance);
+
+            if ((pos.x <= -15 || pos.x >= 15 || pos.z <= 45)
+                && (pos.x <= -15 || pos.x >= 15 || pos.z >= -45)
+                && (pos.x <= -315 || pos.x >= -285 || pos.z <= -155)
+                && (pos.x <= -315 || pos.x >= -285 || pos.z >= -245)
+                && (pos.x >= 177.5 || pos.x <= 147.5 || pos.z <= -255)
+                && (pos.x >= 252.5 || pos.x <= 222.5 || pos.z <= -255)
+                && pos.y <= 30)
+            {
+                gapTrail = trail.positionCount + frequency;
+
+                cube.GetComponent<Rigidbody>().isKinematic = true;
+
+                cube.layer = 1;
+
+                cube.GetComponent<MeshRenderer>().enabled = true;
+
+                isSafe = false;
+                if (!bigWall)
+                {
+                    Instantiate(cube, pos, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(cube, new Vector3(pos.x, pos.y + 2.5f, pos.z), Quaternion.identity);
+                }
+            }
+            else
+            {
+                if (!isSafe)
+                {
+                    tpsSafe = Time.time;
+                    isSafe = true;
+                }
+            }
+        }
+    }
+}
+
